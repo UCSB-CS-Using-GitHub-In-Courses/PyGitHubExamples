2be7e6ac12ea20bff8e03d5e1e20f858a4fdbba8
Phillip Conrad <pconrad@cs.ucsb.edu>
2013-03-27 11:44:40 -0700

    Initial commit of a few PyGithub examples

---
+++ listAllMyRepos.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import getpass
+import sys
+
+# In the main directory of the repo where you are developing with PyGithub,
+# type:
+#    git submodule add git://github.com/jacquev6/PyGithub.git PyGithub
+#    git submodule init
+#    git submodule update
+#
+# That will populate a PyGithub subdirectory with a clone of PyGithub
+# Then, to add it to your Python path, you can do:
+
+sys.path.append("./PyGithub");
+
+from github import Github
+from github import GithubException
+
+username = raw_input("Github Username:")
+pw = getpass.getpass()
+g = Github(username, pw)
+
+for repo in g.get_user().get_repos():
+    print (repo.name)
+
---
+++ listAllReposForAnOrganization.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import getpass
+import sys
+import argparse
+
+# In the main directory of the repo where you are developing with PyGithub,
+# type:
+#    git submodule add git://github.com/jacquev6/PyGithub.git PyGithub
+#    git submodule init
+#    git submodule update
+#
+# That will populate a PyGithub subdirectory with a clone of PyGithub
+# Then, to add it to your Python path, you can do:
+
+sys.path.append("./PyGithub");
+
+from github import Github
+from github import GithubException
+
+
+parser = argparse.ArgumentParser(description='List all repos for an org')
+parser.add_argument('orgName',help='github Organization name')
+
+args = parser.parse_args()
+
+username = raw_input("Github Username:")
+pw = getpass.getpass()
+g = Github(username, pw)
+
+print("All repos for organization: ",args.orgName)
+
+org = g.get_organization(args.orgName)
+
+## TODO: Add some error checking code here to see whether
+##  the lookup was successful.  Do we try/except or check the return value?
+
+repos = org.get_repos()
+
+for repo in repos:
+    print (repo.name)
+
